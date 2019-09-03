f0c77432a57598c3be4a4f05c64a936872d8e1e1
Carl Dungca <carl.dungca@yahoo.com>
2019-09-03 13:10:20 -0700

    Installed sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,8 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-sass`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
