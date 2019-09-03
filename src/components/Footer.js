c5f2d15a2e7f4c44c33f90b09b9a0d035a0a3810
Carl Dungca <carl.dungca@yahoo.com>
2019-09-03 15:25:47 -0700

    Added static resume files to index.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import LinkButton from "./LinkButton"
+import resume from "../assets/resumeFinal.pdf"
 
 const Footer = () => {
 	return (
 		<footer className="content--dark">
 				<h1>Let's make wonderful work together.</h1>
-				<LinkButton link="#">Resume</LinkButton>
+				<LinkButton link={resume}>Resume</LinkButton>
 				<p>carl.dungca@yahoo.com</p>
 				<p>619 - 948 - 8137</p>
 				<a href="https://github.com/CarlsJr4" target="_blank"><i class="fab fa-github"></i></a>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import LinkButton from "../components/LinkButton"
-import Footer from "../components/Footer"
+import resume from "../assets/resumeFinal.pdf"
 // import { Link } from "gatsby"
 
 import Layout from "../components/layout"
@@ -15,8 +15,9 @@ const IndexPage = () => (
 			<h1 id="intro__name">Carl Dungca</h1>
 			<p>Front-end developer who loves problem-solving and functional design.</p>
 			<p>HTML5 - CSS3 - JS - SASS - React</p>
-			<LinkButton link="#">Resume</LinkButton>
+			<LinkButton link={resume}>Resume</LinkButton>
 		</section>
+		{/* The triangle shape at the bottom */}
 		<div className="hero__clip-path"></div>
 
 		{/* ---------- Projects ---------- */}
