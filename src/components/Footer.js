3d462e76b93c92cb398306d23d1332cb9fb530a2
Carl Dungca <carl.dungca@yahoo.com>
2019-09-03 15:14:55 -0700

    Added extra padding to footer button

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
 	return (
 		<footer className="content--dark">
 				<h1>Let's make wonderful work together.</h1>
-				<LinkButton link="#" style={{margin: "1rem"}}>Resume</LinkButton>
+				<LinkButton link="#">Resume</LinkButton>
 				<p>carl.dungca@yahoo.com</p>
 				<p>619 - 948 - 8137</p>
 				<a href="https://github.com/CarlsJr4" target="_blank"><i class="fab fa-github"></i></a>
