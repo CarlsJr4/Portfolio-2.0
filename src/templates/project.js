1545f8b24749703b483541f1e482b61cc5b8a889
Carl Dungca <carl.dungca@yahoo.com>
2019-09-06 13:30:21 -0700

    Loaded project screenshot into template

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import Layout from '../components/layout'
 import SEO from '../components/seo'
+import Linkbutton from '../components/LinkButton'
 import { graphql } from 'gatsby'
 
 
 const project = ({ data }) => {
+	const tools = data.sanityProject.tool
+	const toolString = tools.map( tool => tool.title).join(" - ")
+
 	return (
 		<Layout>
 			<SEO title="project" />
 			<section>
+				<img src={data.sanityProject.screenshot.asset.url} alt={'image of ' + data.sanityProject.title}/>
 				<h1>{data.sanityProject.title}</h1>
+				<p>{toolString}</p>
 			</section>
 		</Layout>
 	);
 }
 
+// Get project information by title (should probably change to something more unique in the future)
 export const query = graphql`
 	query($title: String!) {
 		sanityProject(title: {eq: $title}) {
 			title
+			tool {
+				title
+			}
+			screenshot {
+				asset {
+					url
+				}
+			}
 		}
 	}
 `
