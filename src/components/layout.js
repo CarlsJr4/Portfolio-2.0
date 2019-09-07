10d48aa96ecd0e62943209dd9d7d844c205bb15d
Carl Dungca <carl.dungca@yahoo.com>
2019-09-07 15:27:13 -0700

    Removed missing Header component from Layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,7 +26,6 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
       <div>
         <main>{children}</main>
         <Footer />
